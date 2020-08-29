3215cf172b8a59ffbe74941ca7c91bfcf608e637
shunlab <0kk4024938z195v@au.com>
2020-08-29 12:19:25 +0900

    rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,13 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id              :bigint           not null, primary key
-#  name            :string(255)      not null
-#  password_digest :string(255)      not null
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_name  (name) UNIQUE
-#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe '#age' do
+    context '20年前の生年月日の場合'
+      let(:user) { User.new(birthday: Time.zone.now - 20.years) }
+
+      it '年齢が20歳である事' do
+        expect(user.age).to eq 20
+      end
+    end
+  end
